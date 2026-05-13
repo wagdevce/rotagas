0762d857f5ba032c7a7231e48f7c4097112ae20e
wagner <wmdigitalsolutions@gmail.com>
2026-05-12 23:13:42 -0300

    deploy: finaliza infraestrutura vercel com suporte postgres e bypass pep668

--- core_rotas/wsgi.py
+++ core_rotas/wsgi.py
@@ -1,17 +1,12 @@
-"""
-WSGI config for core_rotas project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Este ficheiro é a ponte entre o servidor web e a tua aplicação Django.
+# O caminho abaixo deve corresponder ao nome da tua pasta de definições.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core_rotas.settings')
 
 application = get_wsgi_application()
+
+# OBRIGATÓRIO PARA A VERCEL:
+# A Vercel procura uma variável chamada 'app' para iniciar o servidor.
 app = application
